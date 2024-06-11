33b2a381d2500398a9bdc9ab8f1485df85774320
Александра <aleksandrasilina@gmail.com>
2024-06-11 12:27:16 +0300

    fixes to main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     database_name = 'coursework_5'
     conn = None
 
-    user_keywords = input("Введите ключевые слова для вывода вакансий через пробел").lower().split()
+    user_keywords = input("Введите ключевые слова через пробел для вывода вакансий").lower().split()
 
     params = config()
     db_manager = DBManager()
@@ -22,35 +22,35 @@ def main():
 
     params.update({'dbname': database_name})
 
+    db_manager.create_employers_table(params)
+    print("Таблица employers успешно создана")
+
+    db_manager.create_vacancies_table(params)
+    print("Таблица vacancies успешно создана")
+
     try:
         with psycopg2.connect(**params) as conn:
             with conn.cursor() as cur:
-                db_manager.create_employers_table(params)
-                print("Таблица employers успешно создана")
-
-                db_manager.create_vacancies_table(params)
-                print("Таблица vacancies успешно создана")
-
                 db_manager.insert_employers_data(cur, hh_api.employers)
                 print("Таблица employers успешно заполнена")
 
                 db_manager.insert_vacancies_data(cur, hh_api.vacancies)
                 print("Таблица vacancies успешно заполнена")
 
-                print("компании и количество вакансий")
+                print("Компании и количество вакансий:")
                 print(db_manager.get_companies_and_vacancies_count(cur))
 
                 print("Вакансии с указанием названия компании, "
-                      "названия вакансии, зарплаты и ссылки на вакансию")
+                      "названия вакансии, зарплаты и ссылки на вакансию:")
                 print(db_manager.get_all_vacancies(cur))
 
-                print("Средняя зарплата по вакансиям")
+                print("Средняя зарплата по вакансиям:")
                 print(db_manager.get_avg_salary(cur))
 
-                print("Вакансии, у которых зарплата выше средней")
+                print("Вакансии, у которых зарплата выше средней:")
                 print(db_manager.get_vacancies_with_higher_salary(cur))
 
-                print("Вакансии, в названии которых содержатся переданные в метод слова")
+                print("Вакансии, в названии которых содержатся ключевые слова:")
                 print(db_manager.get_vacancies_with_keyword(cur, user_keywords))
 
     except (Exception, psycopg2.DatabaseError) as error:
